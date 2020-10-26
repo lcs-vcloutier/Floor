91fec92cc8df49debf209de9b6d13a1138e4f081
Vincent Cloutier <vcloutier@lcs.on.ca>
2020-10-26 16:44:46 -0400

    Initial Commit

---
+++ Floor/FloorApp.swift
@@ -0,0 +1,17 @@
+//
+//  FloorApp.swift
+//  Floor
+//
+//  Created by Cloutier, Vincent on 2020-10-26.
+//
+
+import SwiftUI
+
+@main
+struct FloorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
