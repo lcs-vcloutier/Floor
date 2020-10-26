812147de3f4fc2e7623ddf419df0b51f3444c438
Vincent Cloutier <vcloutier@lcs.on.ca>
2020-10-26 17:12:06 -0400

    added background and started circle button struct

--- Floor/ContentView.swift
+++ Floor/ContentView.swift
@@ -9,12 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("")
+        ZStack{
+            // Create the background
+            Rectangle()
+                .fill(Color("Background"))
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .edgesIgnoringSafeArea(.all)
+            // Add circle button
+            circleButton()
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+struct circleButton: View {
+    var body: some View {
+        Text("...").font(.largeTitle).bold()
     }
 }
